a569fb4a29026940ada41cb5de6ea436cb6a742f
Tara Prasad Tripathy <taraprasad73@gmail.com>
2019-05-14 05:12:52 +0530

    modified circleci config file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['sorting_robot'],
     package_dir={'': 'src'},
-    install_requires=['numpy', 'matplotlib', 'pillow']
+    # install_requires=['numpy', 'matplotlib', 'pillow']
 )
 
 setup(**setup_args)
