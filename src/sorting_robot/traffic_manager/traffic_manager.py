78fc4542b9bcc2a18a710ad892f2f9e68eec1873
Arun Kumar Reddy <arunkumarreddy1997@gmail.com>
2019-05-14 16:30:13 +0100

    modified traffic manager to account for highway_street intersection

--- src/sorting_robot/traffic_manager/traffic_manager.py
+++ src/sorting_robot/traffic_manager/traffic_manager.py
@@ -26,7 +26,7 @@ occupancy_map = [];
 
 
 class StreetSignal:
-    def __init__(self, row, col, rows, cols, directions, k):
+    def __init__(self, row, col, rows, cols, directions, k, wait_time):
         self.row = row;
         self.col = col;
         self.rows = rows;
@@ -37,7 +37,7 @@ class StreetSignal:
         self.vertical = Direction.UP if(Direction.UP in self.directions) else Direction.DOWN;
         self.states = ['GR', 'YR', 'RG', 'RY'];
         self.curr_state = self.states[0];
-        self.wait_time = 5;
+        self.wait_time = wait_time;
 
     def get_signal(self):
         signal = TrafficLight();
@@ -113,7 +113,7 @@ class StreetSignal:
 
 
 class HighwaySignal:
-    def __init__(self, row, col, rows, cols, k):
+    def __init__(self, row, col, rows, cols, k, wait_time):
         self.row = row;
         self.col = col;
         self.rows = rows;
@@ -121,7 +121,7 @@ class HighwaySignal:
         self.k = k;
         self.states = ['GR', 'YR', 'RG', 'RY'];
         self.curr_state = self.states[0];
-        self.wait_time = 5;
+        self.wait_time = wait_time;
 
     def get_signal(self):
         signal = TrafficLight();
@@ -187,6 +187,102 @@ class HighwaySignal:
                 self.curr_state = 'GR';
 
 
+class HybridSignal:
+    def __init__(self, row, col, rows, cols, directions, k, wait_time):
+        self.row = row;
+        self.col = col;
+        self.rows = rows;
+        self.cols = cols;
+        self.k = k;
+        self.directions = directions;
+        if(Direction.LEFT in self.directions):
+            self.horizontal = Direction.LEFT;
+            self.vertical = Direction.UP;
+        elif(Direction.RIGHT in self.directions):
+            self.horizontal = Direction.RIGHT;
+            self.vertical = Direction.DOWN;
+        elif(Direction.UP in self.directions):
+            self.horizontal = Direction.RIGHT;
+            self.vertical = Direction.UP;
+        elif(Direction.DOWN in self.directions):
+            self.horizontal = Direction.LEFT;
+            self.vertical = Direction.DOWN;
+        self.states = ['GR', 'YR', 'RG', 'RY'];
+        self.curr_state = self.states[0];
+        self.wait_time = wait_time;
+
+    def get_signal(self):
+        signal = TrafficLight();
+        signal.left = False;
+        signal.right = False;
+        signal.up = False;
+        signal.down = False;
+        if(self.curr_state == 'GR'):
+            if(self.horizontal == Direction.LEFT):
+                signal.left = True;
+            else:
+                signal.right = True;
+        elif(self.curr_state == 'RG'):
+            if(self.vertical == Direction.UP):
+                signal.up = True;
+            else:
+                signal.down = True;
+        return signal;
+
+    def is_empty(self):
+        return occupancy_map[self.row][self.col];
+
+    def vertical_waiting(self):
+        if(self.vertical == Direction.UP):
+            count = 0;
+            for i in range(self.row + 1, min(self.row + self.k + 1, self.rows)):
+                if(occupancy_map[i][self.col] is True):
+                    count += 1;
+            return count;
+        else:
+            count = 0;
+            for i in range(self.row - 1, max(self.row - self.k - 1, -1), -1):
+                if(occupancy_map[i][self.col] is True):
+                    count += 1;
+            return count;
+
+    def horizontal_waiting(self):
+        if(self.horizontal == Direction.LEFT):
+            count = 0;
+            for i in range(self.col + 1, min(self.col + self.k + 1, self.cols)):
+                if(occupancy_map[self.row][i] is True):
+                    count += 1;
+            return count
+        else:
+            count = 0;
+            for i in range(self.col - 1, min(self.col - self.k - 1, -1), -1):
+                if(occupancy_map[self.row][i] is True):
+                    count += 1;
+            return count;
+
+    def run(self):
+        while not rospy.is_shutdown():
+            if(self.curr_state == 'GR'):
+                if(self.horizontal_waiting() == 0 and self.vertical_waiting() == self.k):
+                    self.curr_state = 'YR';
+                else:
+                    time.sleep(self.wait_time);
+                    self.curr_state = 'YR';
+            elif(self.curr_state == 'YR'):
+                while(self.is_empty() is False):
+                    continue;
+                self.curr_state = 'RG';
+            elif(self.curr_state == 'RG'):
+                if(self.vertical_waiting() == 0 and self.horizontal_waiting() == self.k):
+                    self.curr_state = 'RY';
+                else:
+                    time.sleep(self.wait_time);
+                    self.curr_state = 'RY';
+            elif(self.curr_state == 'RY'):
+                while(self.is_empty() is False):
+                    continue;
+                self.curr_state = 'GR';
+
 class TrafficManager:
     def __init__(self, k, data):
         rospy.init_node('traffic_manager', anonymous=False);
@@ -196,21 +292,26 @@ class TrafficManager:
         for i in range(0, rows):
             occupancy_map.append([False] * cols);
         self.k = k;
+        self.wait_time = 3;
         self.traffic_signals = dict();
         self.threads = [];
         for i in range(rows):
             for j in range(cols):
                 if(data[i][j].cellType == CellType.STREET_STREET_INTERSECTION):
-                    traffic_signal = StreetSignal(i, j, rows, cols, data[i][j].directions, self.k);
+                    traffic_signal = StreetSignal(i, j, rows, cols, data[i][j].directions, self.k, self.wait_time);
                     self.traffic_signals[(i, j)] = traffic_signal;
-                if(data[i][j].cellType == CellType.HIGHWAY_HIGHWAY_INTERSECTION):
+                elif(data[i][j].cellType == CellType.HIGHWAY_HIGHWAY_INTERSECTION):
                     if(self.topleft(i, j, rows, cols) is False):
                         continue;
-                    traffic_signal = HighwaySignal(i, j, rows, cols, self.k);
+                    traffic_signal = HighwaySignal(i, j, rows, cols, self.k, self.wait_time);
                     self.traffic_signals[(i, j)] = traffic_signal;
                     self.traffic_signals[(i, j + 1)] = traffic_signal;
                     self.traffic_signals[(i + 1, j)] = traffic_signal;
                     self.traffic_signals[(i + 1, j + 1)] = traffic_signal;
+                elif(data[i][j].cellType == CellType.HIGHWAY_STREET_INTERSECTION):
+                    if(self.traffic_clash(i,j,rows,cols)==True):
+                        traffic_signal = HybridSignal(i, j, rows, cols, data[i][j].directions, self.k, self.wait_time);
+                        self.traffic_signals[(i, j)] = traffic_signal;
         for key in self.traffic_signals.keys():
             thread = threading.Thread(target=self.traffic_signals[key].run);
             thread.start();
@@ -246,6 +347,27 @@ class TrafficManager:
             return False;
         return True;
 
+    def traffic_clash(self, i, j, rows, cols):
+        def bounds(i, j, rows, cols):
+            return i >= 0 and j >= 0 and i < rows and j < cols;
+
+        if(Direction.LEFT in self.data[i][j].directions):
+            if(bounds(i+1,j,rows,cols) and Direction.UP in self.data[i+1][j].directions);
+                return True;
+            return False;
+        elif(Direction.RIGHT in self.data[i][j].directions):
+            if(bounds(i-1,j,rows,cols) and Direction.DOWN in self.data[i-1][j].directions);
+                return True;
+            return False;
+        elif(Direction.UP in self.data[i][j].directions):
+            if(bounds(i,j-1,rows,cols) and Direction.RIGHT in self.data[i][j-1].directions);
+                return True;
+            return False;
+        elif(Direction.DOWN in self.data[i][j].directions):
+            if(bounds(i,j+1,rows,cols) and Direction.LEFT in self.data[i][j+1].directions);
+                return True;
+            return False;
+
     def visualize(self):
         rows, cols = self.data.shape[0], self.data.shape[1];
         enlarge_ratio = 1;
@@ -261,9 +383,15 @@ class TrafficManager:
                             image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([0, 255, 0]);
                         else:
                             image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([255, 0, 0]);
-                    if(self.data[i][j].cellType == CellType.HIGHWAY_HIGHWAY_INTERSECTION):
+                    elif(self.data[i][j].cellType == CellType.HIGHWAY_HIGHWAY_INTERSECTION):
                         signal = self.traffic_signals[(i, j)].get_signal();
                         if(signal.left is True):
+                            image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([0, 255, 0]);
+                        else:
+                            image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([255, 0, 0]);
+                    elif(self.data[i][j].cellType == CellType.HIGHWAY_STREET_INTERSECTION):
+                        signal = self.traffic_signals[(i, j)].get_signal();
+                        if(signal.left is True or signal.right is True):
                             image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([0, 255, 0]);
                         else:
                             image[i * enlarge_ratio:(i + 1) * enlarge_ratio][j * enlarge_ratio:(j + 1) * enlarge_ratio] = np.array([255, 0, 0]);
@@ -275,15 +403,14 @@ class TrafficManager:
         for i in range(0, len(self.threads)):
             self.threads[i].join();
 
-
 def traffic_manager(k):
     try:
         mapConfiguration = np.load(CONFIG_FILE_LOCATION).item();
         grid = mapConfiguration['grid'];
         manager = TrafficManager(k, grid);
         print("Traffic signals are running");
-        manager.visualize();
-        manager.close();
+        #manager.visualize();
+        #manager.close();
     except IOError:
         print(CONFIG_FILE_LOCATION +
               " doesn't exist. Run the following command to create it:\nrosrun sorting_robot generate_map_config")
